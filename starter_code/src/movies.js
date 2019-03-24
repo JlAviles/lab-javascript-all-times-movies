ba360a6f9ecacffb98df6b3c87aea3c522d23a39
JlAviles <jlavilesmolina@gmail.com>
2019-03-25 00:14:44 +0100

    First iterationcompleted

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,9 +1,23 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
+function turnHoursToMinutes (movies) {
 
+  var newMovies = movies.map(film => {
+    var copy = {...film};
+
+    copy.duration = film.duration.split(" ");
+    hora = copy.duration[0].substring(0,copy.duration[0].length - 1) * 60
+    minutos = copy.duration[1].substring(0,copy.duration[1].length - 3)
+    copy.duration = (parseInt(hora) + parseInt(minutos)) + " minutes";
+    return copy
+  }) 
+  return newMovies
+};
 
 // Get the average of all rates with 2 decimals 
+function ratesAverage () {
 
+}
 
 // Get the average of Drama Movies
 
